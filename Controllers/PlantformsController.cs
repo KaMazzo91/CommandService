be17151d5ee0551feb503a1490b415827dde9e3e
KaMazzo91 <giorgio.mazzolenif@gmail.com>
2024-01-20 17:34:46 +0100

    sqlserverupdate

--- Controllers/PlantformsController.cs
+++ Controllers/PlantformsController.cs
@@ -1,3 +1,6 @@
+using AutoMapper;
+using CommandsService.Data;
+using CommandsService.Dtos;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandsService.Controllers
@@ -6,10 +9,24 @@ namespace CommandsService.Controllers
     [ApiController]
     public class PlatformsController : ControllerBase
     {
-        public PlatformsController()
-        {
+        private readonly ICommandRepo _repository;
+        private readonly IMapper _mapper;
 
+        public PlatformsController(ICommandRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
         }   
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformreadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> getting Platforms from CommandsService");
+            
+            var platformItems = _repository.GetAllPlatforms();
+
+            return Ok(_mapper.Map<IEnumerable<PlatformreadDto>>(platformItems));
+        }
 
         [HttpPost]
         public ActionResult TestInboundConnection()
---
+++ Data/ICommandRepo.cs
@@ -0,0 +1,19 @@
+using CommandsService.Models;
+
+namespace CommandsService.Data
+{
+    public interface ICommandRepo
+    {
+        bool SaveChanges();
+        
+        //Platform
+        IEnumerable<Platform> GetAllPlatforms();
+        void CreatePlatform(Platform plat);
+        bool PlatformExist(int PlatformId);
+
+        //Commands
+        IEnumerable<Command> GetCommandsForPlatform(int platformId);
+        Command GetCommand(int platformId, int commandId);
+        void CreateCommand(int platformId, Command command);
+    }
+}
